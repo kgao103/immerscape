ef4b4f45d8420ee25e7ba0f007e81e52f83b3ba9
hizami-anuar <59573333+hizami-anuar@users.noreply.github.com>
2022-04-03 18:39:22 -0400

    removed useless code

--- app/models.js
+++ app/models.js
@@ -7,16 +7,6 @@ var Cursor = Backbone.Model.extend({
   },
 });
 
-function shuffle(o) {
-  //v1.0
-  for (
-    var j, x, i = o.length;
-    i;
-    j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
-  );
-  return o;
-}
-
 var GameState = Backbone.Model.extend({
   defaults: {
     state: "setup", // setup, playing, end
@@ -196,81 +186,3 @@ wall4 = new Room ({
   background: "img/purple_wall.png",
   items: [dresser],
 })
-
-
-var Ship = Backbone.Model.extend({
-  defaults: {
-    length: 0,
-    isDeployed: false,
-    position: { row: 0, col: 0 },
-    screenPosition: [0, 0],
-    startPosition: [0, 0],
-    screenRotation: 0,
-    isVertical: false,
-    health: 0,
-  },
-
-  initialize: function () {
-    this.set("health", this.get("length"));
-  },
-
-  setScreenPosition: function (position) {
-    this.set("screenPosition", position.slice(0));
-  },
-
-  setScreenRotation: function (rotation) {
-    this.set("screenRotation", rotation);
-  },
-
-  setBoardPosition: function (position) {
-    this.set("position", position);
-  },
-
-  resetShip: function () {
-    this.set("screenPosition", this.get("startPosition").slice(0));
-    this.set("screenRotation", 0);
-    this.set("isVertical", false);
-  },
-
-  snapRotation: function () {
-    var rotation = this.get("screenRotation");
-    var diff1 = Math.abs(rotation - Math.PI / 2);
-    var diff2 = Math.abs(rotation + Math.PI / 2);
-    var isVertical = diff1 < Math.PI / 4 || diff2 < Math.PI / 4;
-    this.set("screenRotation", isVertical ? Math.PI / 2 : 0);
-    this.set("isVertical", isVertical);
-  },
-
-  getEndpoints: function () {
-    var endpoint = {
-      row: this.get("position").row,
-      col: this.get("position").col,
-    };
-    if (this.get("isVertical")) endpoint.row += this.get("length") - 1;
-    else endpoint.col += this.get("length") - 1;
-    return { start: this.get("position"), end: endpoint };
-  },
-
-  getScreenOrigin: function () {
-    var origin = this.get("screenPosition").slice(0);
-    if (this.get("isVertical")) {
-      // Get vertical origin
-      origin[0] += (this.get("length") * TILESIZE) / 2;
-      origin[1] -= (this.get("length") * TILESIZE) / 2;
-    }
-    return origin;
-  },
-
-  overlaps: function (otherShip) {
-    var a = this.getEndpoints();
-    var b = otherShip.getEndpoints();
-
-    return (
-      a.start.row <= b.end.row &&
-      a.end.row >= b.start.row &&
-      a.start.col <= b.end.col &&
-      a.end.col >= b.start.col
-    );
-  },
-});
-var ShipSet = Backbone.Collection.extend({ model: Ship });
